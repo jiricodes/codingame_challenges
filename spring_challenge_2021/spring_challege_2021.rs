938474d55129910df0dfdcf9a1c9e7353c6a9b85
Jiri Novotny <jnovotny@student.hive.fi>
2021-05-06 22:29:28 +0300

    Update spring_challege_2021.rs

--- spring_challenge_2021/spring_challege_2021.rs
+++ spring_challenge_2021/spring_challege_2021.rs
@@ -16,7 +16,7 @@ macro_rules! parse_input {
 }
 // Constants
 const TREE_SUN_POINTS: i32 = 3;
-const TREE_LIFE_COST: i32 = 4;
+const TREE_LIFECYCLE_COST: i32 = 4;
 const FOREST_INITIAL_NUTRIENT: i32 = 20;
 const POINTS_PER_3SUN: i32 = 1;
 const GAME_LENGTH: i32 = 24;
@@ -79,12 +79,33 @@ impl Board {
 
 // Tree
 struct Tree {
-    owner: Player,
     sun_points: i32,
+    cost: i32,
     cell_index: i32,
     size: i32,
     is_mine: bool,
     is_dormant: bool,
+}
+
+impl Tree {
+    pub fn new() -> Tree {
+        let mut new_tree = Tree {
+            sun_points: TREE_SUN_POINTS,
+            cost: TREE_LIFECYCLE_COST,
+            cell_index: 0,
+            size: 0,
+            is_mine: false,
+            is_dormant: false,
+        };
+        let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            new_tree.cell_index = parse_input!(inputs[0], i32); // location of this tree
+            new_tree.size = parse_input!(inputs[1], i32); // size of this tree: 0-3
+            new_tree.is_mine = parse_input!(inputs[2], i32) != 0; // 1 if this is your tree
+            new_tree.is_dormant = parse_input!(inputs[3], i32) != 0; // 1 if this tree is dormant
+        return new_tree;
+    }
 }
 
 // Player - is it needed?
@@ -118,8 +139,9 @@ impl Player {
 }
 
 // Actions
-struct Actions {
+struct Action {
     action_string: String,
+    cell_index: i32,
 }
 
 // Game
@@ -127,7 +149,8 @@ struct Game {
     day: i32,
     nutrients: i32,
     board: Board,
-    actions: Vec<Actions>,
+    trees: Vec<Tree>,
+    actions: Vec<Action>,
     me: Player,
     opponent: Player,
 }
@@ -145,6 +168,7 @@ impl Game {
             day: 0,
             nutrients: FOREST_INITIAL_NUTRIENT,
             board: board,
+            trees: Vec::new(),
             actions: Vec::new(),
             me: Player::new(),
             opponent: Player::new(),
@@ -152,6 +176,7 @@ impl Game {
     }
 
     pub fn update(&mut self) {
+        self.reset();
         // Consider using input_line.clear() instead of new all the time
 
         let mut input_line = String::new();
@@ -164,7 +189,19 @@ impl Game {
 
         self.me.update();
         self.opponent.update();
-    } 
+
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let number_of_trees = parse_input!(input_line, i32); // the current amount of trees
+        for i in 0..number_of_trees as usize {
+           self.trees.push(Tree::new());
+        }
+    }
+
+    fn reset(&mut self) {
+        self.trees.clear();
+        self.actions.clear();
+    }
 }
 
 
@@ -179,19 +216,6 @@ fn main() {
     // game loop
     loop {
         game.update();
-
-        let mut input_line = String::new();
-        io::stdin().read_line(&mut input_line).unwrap();
-        let number_of_trees = parse_input!(input_line, i32); // the current amount of trees
-        for i in 0..number_of_trees as usize {
-            let mut input_line = String::new();
-            io::stdin().read_line(&mut input_line).unwrap();
-            let inputs = input_line.split(" ").collect::<Vec<_>>();
-            let cell_index = parse_input!(inputs[0], i32); // location of this tree
-            let size = parse_input!(inputs[1], i32); // size of this tree: 0-3
-            let is_mine = parse_input!(inputs[2], i32); // 1 if this is your tree
-            let is_dormant = parse_input!(inputs[3], i32); // 1 if this tree is dormant
-        }
 
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
