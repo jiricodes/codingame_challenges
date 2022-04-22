5e04cd32415b7025fddb0abc408322ef42f3a583
Jiri Novotny <novotny.049@gmail.com>
2022-04-22 08:23:28 +0300

    remove and ignore rust build

--- spring_challenge_2022/target/debug/deps/spring_challenge_2022-be7e904a4113abe4.d
+++
@@ -1,6 +0,0 @@
-/Users/jnovotny/Documents/codingame_challenges/spring_challenge_2022/target/debug/deps/spring_challenge_2022-be7e904a4113abe4.rmeta: src/main.rs src/tests.rs
-
-/Users/jnovotny/Documents/codingame_challenges/spring_challenge_2022/target/debug/deps/spring_challenge_2022-be7e904a4113abe4.d: src/main.rs src/tests.rs
-
-src/main.rs:
-src/tests.rs:
