34abfe60492141adf873a3f1b7acd95dad0e4c12
Jiri Novotny <novotny.049@gmail.com>
2025-04-07 11:07:22 +0300

    initial version, some tests pass

---
+++ spring_challenge_2025/src/main.rs
@@ -0,0 +1,322 @@
+use std::fmt;
+use std::fmt::Display;
+use std::io;
+use std::ops::Add;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
+}
+
+#[derive(Debug, Clone, Copy)]
+struct StateHash {
+    hash: i32,
+}
+
+impl From<State> for StateHash {
+    fn from(value: State) -> Self {
+        let mut hash: i32 = 0;
+        for i in 0..9 {
+            hash = hash * 10 + value.tiles[i] as i32;
+        }
+        Self { hash }
+    }
+}
+
+impl From<&State> for StateHash {
+    fn from(value: &State) -> Self {
+        let mut hash: i32 = 0;
+        for i in 0..9 {
+            hash = hash * 10 + value.tiles[i] as i32;
+        }
+        Self { hash }
+    }
+}
+
+impl Default for StateHash {
+    fn default() -> Self {
+        Self { hash: 0 }
+    }
+}
+
+const HASHMOD: i32 = 1 << 30;
+const BITWISE_HASHMOD: i32 = 0b111111111111111111111111111111;
+
+impl Add<StateHash> for StateHash {
+    type Output = Self;
+    fn add(self, rhs: StateHash) -> Self::Output {
+        Self {
+            hash: (self.hash + rhs.hash) & BITWISE_HASHMOD,
+        }
+    }
+}
+
+impl Display for StateHash {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.hash)
+    }
+}
+
+struct Tile {
+    val: u8,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct State {
+    tiles: [u8; 9],
+}
+
+impl State {
+    const NGBS_0: [u8; 2] = [1, 3];
+    const NGBS_1: [u8; 3] = [0, 2, 4];
+    const NGBS_2: [u8; 2] = [1, 5];
+    const NGBS_3: [u8; 3] = [0, 4, 6];
+    const NGBS_4: [u8; 4] = [1, 3, 5, 7];
+    const NGBS_5: [u8; 3] = [2, 4, 8];
+    const NGBS_6: [u8; 2] = [3, 7];
+    const NGBS_7: [u8; 3] = [4, 6, 8];
+    const NGBS_8: [u8; 2] = [5, 7];
+    const NGBS_0_PERMS: [&[usize]; 1] = [&[1, 3]];
+    const NGBS_1_PERMS: [&[usize]; 4] = [&[0, 2], &[0, 4], &[2, 4], &[0, 2, 4]];
+    const NGBS_2_PERMS: [&[usize]; 1] = [&[1, 5]];
+    const NGBS_3_PERMS: [&[usize]; 4] = [&[0, 4], &[0, 6], &[4, 6], &[0, 4, 6]];
+    const NGBS_4_PERMS: [&[usize]; 11] = [
+        &[1, 3],
+        &[1, 5],
+        &[1, 7],
+        &[3, 5],
+        &[3, 7],
+        &[5, 7],
+        &[1, 3, 5],
+        &[1, 3, 7],
+        &[1, 5, 7],
+        &[3, 5, 7],
+        &[1, 3, 5, 7],
+    ];
+    const NGBS_5_PERMS: [&[usize]; 4] = [&[2, 4], &[2, 8], &[4, 8], &[2, 4, 8]];
+    const NGBS_6_PERMS: [&[usize]; 1] = [&[3, 7]];
+    const NGBS_7_PERMS: [&[usize]; 4] = [&[4, 6], &[4, 8], &[6, 8], &[4, 6, 8]];
+    const NGBS_8_PERMS: [&[usize]; 1] = [&[5, 7]];
+    const HARDCODE_NGBS: [&'static [&'static [usize]]; 9] = [
+        &Self::NGBS_0_PERMS,
+        &Self::NGBS_1_PERMS,
+        &Self::NGBS_2_PERMS,
+        &Self::NGBS_3_PERMS,
+        &Self::NGBS_4_PERMS,
+        &Self::NGBS_5_PERMS,
+        &Self::NGBS_6_PERMS,
+        &Self::NGBS_7_PERMS,
+        &Self::NGBS_8_PERMS,
+    ];
+    fn from_io() -> Self {
+        let mut tiles: [u8; 9] = [0; 9];
+        for i in 0..3 as usize {
+            let mut inputs = String::new();
+            io::stdin().read_line(&mut inputs).unwrap();
+            for (j, txt) in inputs.split_whitespace().enumerate() {
+                let value = parse_input!(txt, u8);
+                tiles[i * 3 + j] = value;
+            }
+        }
+        Self { tiles }
+    }
+
+    fn try_capture(&self, placement: usize, ngbs: &[usize]) -> Option<Self> {
+        let (cnt, ttl) = ngbs.iter().fold((0, 0), |acc, n| {
+            (acc.0 + (self.tiles[*n] != 0) as u8, acc.1 + self.tiles[*n])
+        });
+        if cnt > 1 && ttl <= 6 {
+            let mut new = Self {
+                tiles: self.tiles.clone(),
+            };
+            new.tiles[placement] = ttl;
+            for n in ngbs {
+                new.tiles[*n] = 0;
+            }
+            Some(new)
+        } else {
+            None
+        }
+    }
+
+    fn next_states(&self, placement: usize) -> Vec<Self> {
+        let mut ret = Vec::new();
+        for ngbs in Self::HARDCODE_NGBS[placement] {
+            if let Some(new) = self.try_capture(placement, ngbs) {
+                ret.push(new);
+            }
+        }
+        if ret.len() == 0 {
+            let mut new = Self {
+                tiles: self.tiles.clone(),
+            };
+            new.tiles[placement] = 1;
+            ret.push(new);
+        }
+        ret
+    }
+
+    fn solve(&self, depth: i32) -> i32 {
+        let mut current: Vec<State> = vec![self.clone()];
+        let mut results: Vec<StateHash> = Vec::new();
+        for d in 0..depth {
+            // if we don't have a state in the queue, we ran out of the options
+            if current.is_empty() {
+                eprintln!("stopped at d={}", d);
+                break;
+            }
+            //dbg!(&current);
+            // create a new queue, that will serve as next iteration with increased depth
+            // we can perhaps cull here?
+            let mut new: Vec<State> = Vec::new();
+            // iterate over all states at this depth
+            for state in current.iter() {
+                let mut is_finished = true;
+                // for each state check all possible placements
+                for i in 0..9 {
+                    if state.tiles[i] != 0 {
+                        continue;
+                    }
+                    is_finished = false;
+                    let mut next_states = state.next_states(i);
+                    // dbg!(next_states.len());
+                    // dbg!(&next_states);
+                    new.append(&mut next_states);
+                    // for each capture, queue up next possible state
+                    // or place one
+                }
+                if is_finished {
+                    results.push(StateHash::from(state));
+                }
+            }
+            current = new;
+        }
+        // append the state att current depth, but not finished games
+        for state in current {
+            results.push(StateHash::from(state));
+        }
+        // iter over results and sum hashes
+        dbg!(results.len());
+        let ret = results.iter().fold(StateHash::default(), |acc, x| acc + *x);
+        ret.hash
+    }
+
+    //change toggle
+    //for each tile
+    //if not empty then
+    //continue
+    //else
+    //check for neighbors and capture
+    //else add 1
+    //que the new state
+    //toggle = true
+    //continue
+    //if toggle false, record state (hash)
+
+    fn eprint(&self) {
+        eprintln!("{} | {} | {}", self.tiles[0], self.tiles[1], self.tiles[2]);
+        eprintln!("{} | {} | {}", self.tiles[3], self.tiles[4], self.tiles[5]);
+        eprintln!("{} | {} | {}", self.tiles[6], self.tiles[7], self.tiles[8]);
+    }
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{} | {} | {} | {} | {} | {} | {} | {} | {}",
+            self.tiles[0],
+            self.tiles[1],
+            self.tiles[2],
+            self.tiles[3],
+            self.tiles[4],
+            self.tiles[5],
+            self.tiles[6],
+            self.tiles[7],
+            self.tiles[8],
+        )
+    }
+}
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let depth = parse_input!(input_line, i32);
+    dbg!(depth);
+    let state = State::from_io();
+    // state.eprint();
+    // for i in 0..3 as usize {
+    //     let mut inputs = String::new();
+    //     io::stdin().read_line(&mut inputs).unwrap();
+    //     for j in inputs.split_whitespace() {
+    //         let value = parse_input!(j, i32);
+    //     }
+    // }
+
+    // Write an action using println!("message...");
+    // To debug: eprintln!("Debug message...");
+
+    println!("{}", state.solve(depth));
+}
+
+#[cfg(test)]
+mod tests {
+    use std::i32;
+
+    use super::*;
+    #[test]
+    fn hash() {
+        let state = State {
+            tiles: [1, 2, 3, 4, 5, 6, 0, 1, 2],
+        };
+        let hash = StateHash::from(state);
+        assert_eq!(hash.hash, 123456012);
+    }
+
+    #[test]
+    fn correct_mod() {
+        assert_eq!(HASHMOD, 0b1000000000000000000000000000000);
+
+        let n = 123456012;
+        assert_eq!(n % HASHMOD, n & BITWISE_HASHMOD);
+        let n = i32::MAX - 2;
+        assert_eq!(n % HASHMOD, n & BITWISE_HASHMOD);
+    }
+
+    #[test]
+    fn two_states() {
+        let depth = 20;
+        let state = State {
+            tiles: [0, 6, 0, 2, 2, 2, 1, 6, 1],
+        };
+        let res = state.solve(depth);
+        let expected = 322444322;
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn two_unique_states() {
+        let depth = 1;
+        let state = State {
+            tiles: [5, 5, 5, 0, 0, 5, 5, 5, 5],
+        };
+        let res = state.solve(depth);
+        let expected = 36379286;
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn unique_states_20() {
+        let depth = 8;
+        let state = State {
+            tiles: [6, 0, 6, 0, 0, 0, 6, 1, 5],
+        };
+        let res = state.solve(depth);
+        let expected = 76092874;
+        assert_eq!(res, expected);
+    }
+}
